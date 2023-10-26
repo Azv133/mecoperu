eb9e8330f28f692ff0faf91e7426a567e36d1bb3
Azv133 <adrianzavalevega@gmail.com>
2023-10-26 00:28:38 -0500

    Primera versión

---
+++ script.js
@@ -0,0 +1,15 @@
+
+console.log("testing")
+
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 420,
+    grabCursor: false,
+    speed: 800,
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+});
+
+
