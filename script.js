be4c70d382c7fa207e74d50ed898184c504fb9f3
Azv133 <adrianzavalevega@gmail.com>
2023-10-29 00:33:45 -0500

    Responsive

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-
-console.log("testing")
-
 var swiper = new Swiper(".swiper", {
     slidesPerView: 1,
     spaceBetween: 420,
     grabCursor: false,
     speed: 800,
+    loop: true,
     autoplay: {
         delay: 1500,
         disableOnInteraction: false,
