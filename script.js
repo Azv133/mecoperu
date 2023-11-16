6959bde2aea0d9759466a6cc4371a17d734a8c59
Adrian Z <adrianzavalevega@gmail.com>
2023-11-15 23:59:42 -0500

    Útimos ajustes

--- script.js
+++ script.js
@@ -5,7 +5,29 @@ var swiper = new Swiper(".swiper", {
     speed: 800,
     loop: true,
     autoplay: {
-        delay: 1500,
+        delay: 5000,
         disableOnInteraction: false,
     },
 });
+
+window.onload = function() {
+    var enlaces = document.querySelectorAll('a[href^="#"]');
+
+    enlaces.forEach(function(enlace) {
+        enlace.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            var seccionID = this.getAttribute('href').substring(1);
+            var seccion = document.getElementById(seccionID);
+            var seccionRect = seccion.getBoundingClientRect();
+
+            var posicionCentro = (window.innerHeight / 2) - (seccionRect.height / 2);
+            var posicionFinal = seccionRect.top + window.scrollY - posicionCentro;
+
+            window.scrollTo({
+                top: posicionFinal,
+                behavior: 'smooth'
+            });
+        });
+    });
+};
